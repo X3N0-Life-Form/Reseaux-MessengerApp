c1e7aa3a0273ae2b7d837aea5307fa23e39eb300
wyrllyn <etudiant@LKQ.localdomain>
2013-10-24 09:54:42 +0200

    gui test

---
+++ src/client/GUI/ContactPanel.java
@@ -0,0 +1,27 @@
+package client.GUI;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import java.awt.Color;
+import java.awt.Dimension;
+
+public class ContactPanel extends JPanel{
+	
+	JButton refresh = new JButton("refresh");
+	public ContactPanel(){
+		setBackground(Color.BLUE);
+		setVisible(true);
+	}
+
+	//TODO : afficher liste contacts, bouton refresh, tout ça tout ça
+
+
+}
+
+class SeeContactPanel{	
+	public static void main (String[] args){
+		new ContactPanel();
+	}
+}
+
+
