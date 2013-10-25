21a1e7a5853fd345dfd14374c34470029d52a46a
X3N0-Life-Form <adrarsis@laposte.net>
2013-10-25 12:09:21 +0200

    added getters & setters to client.Client

--- src/client/Client.java
+++ src/client/Client.java
@@ -5,7 +5,40 @@ public class Client {
 	private String ip;
 	private int port;
 	
+	public Client(String login, String ip, int port) {
+		super();
+		this.login = login;
+		this.ip = ip;
+		this.port = port;
+	}
+
 	public static void main(String args[]) {
 		
 	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getIp() {
+		return ip;
+	}
+
+	public void setIp(String ip) {
+		this.ip = ip;
+	}
+
+	public int getPort() {
+		return port;
+	}
+
+	public void setPort(int port) {
+		this.port = port;
+	}
+	
+	
 }
