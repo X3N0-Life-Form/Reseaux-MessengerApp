c17b6374301bc0f52e8285a0a111f6c3b0b73c04
X3N0-Life-Form <adrarsis@laposte.net>
2013-11-30 02:45:50 +0100

    even more javadoc

--- src/client/handling/TCPHandlerClient.java
+++ src/client/handling/TCPHandlerClient.java
@@ -5,7 +5,6 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.DatagramSocket;
 import java.net.Socket;
-import java.net.SocketException;
 
 import client.Client;
 import client.ClientMessageManager;
@@ -13,18 +12,36 @@ import client.ClientMessageManager;
 import commun.Message;
 import commun.MessageType;
 
+/**
+ * Client side TCP handler class. Sends the Client's login and password to the Server for
+ * authentication. Note that despite possessing a run() method, this is <b>not</b> a Thread.
+ * @author etudiant
+ * @see ClientMessageManager
+ */
 public class TCPHandlerClient implements HandlerClient {
 	
 	private Socket socket;
 	private Client client;
 	private ClientMessageManager messageManager;
 	
+	/**
+	 * Constructs a TCPHandler Object for the specified client using the specified Socket. Note
+	 * that the TCP dialog must be launched by the Client by calling the run() method.
+	 * @param clientSocket
+	 * @param client
+	 */
 	public TCPHandlerClient(Socket clientSocket, Client client) {
 		socket = clientSocket;
 		this.client = client;
 		messageManager = new ClientMessageManager(client,this);
 	}
 	
+	/**
+	 * Sends a connection request to the Server, using the Client's login and password.
+	 * @throws IOException
+	 * @throws ClassNotFoundException
+	 * @throws HandlingException
+	 */
 	public void handleConnect() throws IOException, ClassNotFoundException, HandlingException {
 		Message message = new Message(MessageType.CONNECT);
 		message.addInfo("login",client.getLogin());
@@ -32,25 +49,42 @@ public class TCPHandlerClient implements HandlerClient {
 		this.sendMessage(message,socket);
 	}
 
+	/**
+	 * Receives messages from the Server and passes them to the MessageManager. In effect,
+	 * this should only be receiving answers to a connection request. 
+	 * @throws IOException
+	 * @throws ClassNotFoundException
+	 * @throws HandlingException
+	 */
 	public void handleDialog() throws IOException, ClassNotFoundException, HandlingException {
 		Message message = getServeurMessage(socket);
 		messageManager.handleMessage(message, socket);
 		socket.close();
 	}
 
-	
+	@Override
 	public void sendMessage(Message message, Socket socket) throws IOException {
 		ObjectOutputStream os = new ObjectOutputStream(socket.getOutputStream());
 		os.writeObject(message);
 		os.flush();
 	}
 
+	/**
+	 * Gets a Message received through the specified socket.
+	 * @param socket
+	 * @return Message object.
+	 * @throws IOException
+	 * @throws ClassNotFoundException
+	 */
 	public Message getServeurMessage(Socket socket) throws IOException, ClassNotFoundException {
 		ObjectInputStream is = new ObjectInputStream(socket.getInputStream());
 		Message message = (Message) is.readObject();
 		return message;
 	}
 
+	/**
+	 * Connects the Client to the Server.
+	 */
 	public void run() {
 		try {
 			handleConnect();
@@ -64,6 +98,7 @@ public class TCPHandlerClient implements HandlerClient {
 		}
 	}
 
+	@Override
 	public Client getClient() {
 		return client;
 	}
