f1ddc117d0a46bd13ae6fcf804d1bd9a13344b06
X3N0-Life-Form <adrarsis@laposte.net>
2013-12-07 00:50:52 +0100

    refactoring: removed "GENERIC" from constant names

--- src/common/MessageInfoStrings.java
+++ src/common/MessageInfoStrings.java
@@ -8,9 +8,9 @@ package common;
 public class MessageInfoStrings {
 	public static final String REQUEST_IP_TARGET_PORT = "targetPort";
 	public static final String REQUEST_IP_TARGET_IP = "targetIp";
-	public static final String GENERIC_PORT = "port";
-	public static final String GENERIC_PASSWORD = "pass";
-	public static final String GENERIC_LOGIN = "login";
+	public static final String PORT = "port";
+	public static final String PASSWORD = "pass";
+	public static final String LOGIN = "login";
 	public static final String REQUEST_LIST_CLIENT_LOGINS = "clientLogins";
 
 	/**
