1ba3fb6067bd0cfc028beb451930f2362176271c
X3N0-Life-Form <adrarsis@laposte.net>
2013-12-10 17:13:10 +0100

    deco/reco: unit test passes, manual doesn't

--- test/common/ConnectionManagement.java
+++ test/common/ConnectionManagement.java
@@ -10,9 +10,9 @@ import org.jdom2.JDOMException;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.BeforeClass;
-import org.junit.Ignore;
 import org.junit.Test;
 
+import common.handling.HandlingException;
 import common.logging.EventType;
 import client.Client;
 import client.handling.TCPHandlerClient;
@@ -29,7 +29,6 @@ public class ConnectionManagement {
 	
 	private Client client;
 	private TCPHandlerClient tcpHandlerClient;
-	@SuppressWarnings("unused")
 	private UDPClient udpClient;
 	
 	private Client client_2;
@@ -67,6 +66,13 @@ public class ConnectionManagement {
 		server.stop();
 	}
 
+	/**
+	 * Clients shouldn't be able to connect twice.
+	 * Manual test result (10/12/13 - 14h33): OK
+	 * @throws IOException
+	 * @throws JDOMException
+	 * @throws InterruptedException
+	 */
 	@Test
 	public void testClientConnectsTwice() throws IOException, JDOMException, InterruptedException {
 		Message connectMessage = new Message(MessageType.CONNECT);
@@ -94,27 +100,42 @@ public class ConnectionManagement {
 		//assertFalse(client_2.getLog().getEventsByType(EventType.ERROR).isEmpty());
 	}
 	
-	@Ignore //redundant
+	/**
+	 * A client must be able to disconnect and reconnect without issues.
+	 * Manual test result (10/12/13 17h11): FAILED
+	 * @throws IOException
+	 * @throws InterruptedException
+	 * @throws HandlingException
+	 * @throws ClassNotFoundException
+	 */
 	@Test
-	public void testClientAlreadyConnected() throws IOException, InterruptedException {
+	public void testClientDisconnectsThenReconnects() throws IOException, InterruptedException, HandlingException, ClassNotFoundException {
 		Message connectMessage = new Message(MessageType.CONNECT);
 		connectMessage.addInfo(MessageInfoStrings.LOGIN, "test01");
 		connectMessage.addInfo(MessageInfoStrings.PASSWORD, "test");
 		Socket socket = client.getClientSocket();
-		server.getClientIps().put("test01", null);
 		
-		socket.connect(new InetSocketAddress(client.getServerIp(), serverPort), 10000);
+		socket.connect(new InetSocketAddress(client.getServerIp(), serverPort), 10000);//copied from TCPHandlerClient.run()
 		
 		tcpHandlerClient.sendMessage(connectMessage, socket);
-		//Thread.sleep(100);
-		//System.out.println(server.getLog().getEvents());
-		//System.out.println(client.getLog().getEvents());
-	}
-	
-	@Ignore
-	@Test
-	public void testClientDisconnectsThenReconnects() {
-	
+		Thread.sleep(100);
+		assertFalse(server.getLog().getEventsByType(EventType.RECEIVE_TCP).isEmpty());
+		assertTrue(server.getClientIps().containsKey("test01"));
+		
+		Message disconnectMessage = new Message(MessageType.DISCONNECT);
+		disconnectMessage.addInfo(MessageInfoStrings.LOGIN, "test01");
+		udpClient.getSend().sendMessage(disconnectMessage, udpClient.getSend().getSocket());
+		Thread.sleep(100);
+		assertFalse(server.getClientIps().containsKey("test01"));
+		
+		client_2 = new Client("test01", "test", "localhost", serverPort);
+		client_2.setupHandlers();
+		tcpHandlerClient_2 = client_2.getTcpHandlerClient();
+		Socket socket_2 = client_2.getClientSocket();
+		socket_2.connect(new InetSocketAddress(client_2.getServerIp(), serverPort), 10000);
+		tcpHandlerClient_2.sendMessage(connectMessage, socket_2);
+		Thread.sleep(100);
+		assertTrue(server.getClientIps().containsKey("test01"));
 	}
 
 }
